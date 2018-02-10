d80faee2f7febc7a530e7b1d19cf92aee541bd57
Cameron Moseley <cmoseley@gmail.com>
2018-02-10 10:20:14 -0800

    Implement Monsters Tab
    - Implemented Monsters tab with New, Save, and Load XML
    - Created MonsterDialog form

--- HybrasylXmlEditor/UI/MainForm.Designer.cs
+++ HybrasylXmlEditor/UI/MainForm.Designer.cs
@@ -31,12 +31,14 @@
             this.MainFormMenuStrip = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabControlXmlEditor = new System.Windows.Forms.TabControl();
             this.tabPageStatus = new System.Windows.Forms.TabPage();
             this.tabPageNpc = new System.Windows.Forms.TabPage();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPageMonster = new System.Windows.Forms.TabPage();
+            this.tabPageSpawnGroup = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.MainFormMenuStrip.SuspendLayout();
-            this.tabControl1.SuspendLayout();
+            this.tabControlXmlEditor.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainFormMenuStrip
@@ -61,20 +63,22 @@
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
-            // tabControl1
+            // tabControlXmlEditor
             // 
-            this.tabControl1.Controls.Add(this.tabPageStatus);
-            this.tabControl1.Controls.Add(this.tabPageNpc);
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Location = new System.Drawing.Point(0, 28);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(900, 930);
-            this.tabControl1.TabIndex = 1;
+            this.tabControlXmlEditor.Controls.Add(this.tabPageStatus);
+            this.tabControlXmlEditor.Controls.Add(this.tabPageNpc);
+            this.tabControlXmlEditor.Controls.Add(this.tabPageMonster);
+            this.tabControlXmlEditor.Controls.Add(this.tabPageSpawnGroup);
+            this.tabControlXmlEditor.Controls.Add(this.tabPage2);
+            this.tabControlXmlEditor.Location = new System.Drawing.Point(0, 28);
+            this.tabControlXmlEditor.Name = "tabControlXmlEditor";
+            this.tabControlXmlEditor.SelectedIndex = 0;
+            this.tabControlXmlEditor.Size = new System.Drawing.Size(900, 930);
+            this.tabControlXmlEditor.TabIndex = 1;
             // 
             // tabPageStatus
             // 
@@ -96,22 +100,42 @@
             this.tabPageNpc.Text = "NPC";
             this.tabPageNpc.UseVisualStyleBackColor = true;
             // 
-            // tabPage1
+            // tabPageMonster
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(892, 904);
-            this.tabPage1.TabIndex = 2;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPageMonster.Location = new System.Drawing.Point(4, 22);
+            this.tabPageMonster.Name = "tabPageMonster";
+            this.tabPageMonster.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMonster.Size = new System.Drawing.Size(892, 904);
+            this.tabPageMonster.TabIndex = 2;
+            this.tabPageMonster.Text = "Monster";
+            this.tabPageMonster.UseVisualStyleBackColor = true;
+            // 
+            // tabPageSpawnGroup
+            // 
+            this.tabPageSpawnGroup.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSpawnGroup.Name = "tabPageSpawnGroup";
+            this.tabPageSpawnGroup.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageSpawnGroup.Size = new System.Drawing.Size(892, 904);
+            this.tabPageSpawnGroup.TabIndex = 3;
+            this.tabPageSpawnGroup.Text = "SpawnGroup";
+            this.tabPageSpawnGroup.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(892, 904);
+            this.tabPage2.TabIndex = 4;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(902, 959);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tabControlXmlEditor);
             this.Controls.Add(this.MainFormMenuStrip);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.MainFormMenuStrip;
@@ -121,7 +145,7 @@
             this.Text = "Hybrasyl Xml Editor";
             this.MainFormMenuStrip.ResumeLayout(false);
             this.MainFormMenuStrip.PerformLayout();
-            this.tabControl1.ResumeLayout(false);
+            this.tabControlXmlEditor.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,10 +156,12 @@
         private System.Windows.Forms.MenuStrip MainFormMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl tabControlXmlEditor;
         private System.Windows.Forms.TabPage tabPageStatus;
         private System.Windows.Forms.TabPage tabPageNpc;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPageMonster;
+        private System.Windows.Forms.TabPage tabPageSpawnGroup;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
---
+++ HybrasylXmlEditor/UI/ViewModel/MonsterViewModel.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * This file is part of Project Hybrasyl.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the Affero General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * without ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * (C) 2016 Project Hybrasyl (info@hybrasyl.com)
+ *
+ */
+
+using Hybrasyl.Creatures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HybrasylXmlEditor.UI.ViewModel
+{
+    public class MonsterViewModel : INotifyPropertyChanged
+    {
+
+        private string _name;
+        private string _description;
+        private ushort _sprite;
+
+        public Creature Monster { get; set; }
+
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged(); } }
+        public string Description { get { return _description; } set { _description = value; OnPropertyChanged(); } }
+        public ushort Sprite { get { return _sprite; } set { _sprite = value; OnPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MonsterViewModel(Creature monster)
+        {
+            this.Monster = monster;
+        }
+
+        public void SetDisplayMonster(Creature monster)
+        {
+            this.Name = monster.Name;
+            this.Description = monster.Description;
+            this.Sprite = monster.Sprite;
+        }
+
+        public Creature GetDisplayMonster()
+        {
+            var newMonster = new Creature();
+            newMonster.Name = this.Name;
+            newMonster.Description = this.Description;
+            newMonster.Sprite = this.Sprite;
+
+            return newMonster;
+
+            //return new Creature
+            //{
+            //    Name = this.Name,
+            //    Description = this.Description,
+            //    Sprite = this.Sprite
+            //};
+        }
+        
+
+
+
+    }
+}
